898d7866fda92acdfa3a98862539e679ccc95a5f
heffar <s_salim42@hotmail.fr>
2020-04-18 18:26:17 +0200

    Editing product implementation

--- 9.Dynamic Routes and Models/controllers/admin.js
+++ 9.Dynamic Routes and Models/controllers/admin.js
@@ -16,7 +16,7 @@ exports.postAddProduct = (req,res,next) => {
     const price = req.body.price;
     const description = req.body.description;
     
-    const product = new Product(title, imageUrl, description, price);
+    const product = new Product(null, title, imageUrl, description, price);
 
     product.save();
     res.redirect("/");
@@ -43,6 +43,19 @@ exports.getEditProduct = (req, res, next) => {
         }
     })
 }
+
+exports.postEditProduct = (req, res) => {
+    const prodId = req.body.productId;
+    const updatedTtile = req.body.title;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDescription = req.body.description;
+    const updatedPrice = req.body.price;
+
+    const updatedProduct = new Product(prodId, updatedTtile, updatedImageUrl, updatedDescription, updatedPrice);
+    updatedProduct.save();
+    res.redirect('/admin/products');
+     
+};
 
 exports.getProducts = (req, res) => {
     Product.fetchAll((products) => {
