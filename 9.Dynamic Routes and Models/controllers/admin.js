043cc1c49923ed54376043492d7c9b8ee521a3aa
heffar <s_salim42@hotmail.fr>
2020-04-18 19:19:27 +0200

    Deleting product and from cart too

--- 9.Dynamic Routes and Models/controllers/admin.js
+++ 9.Dynamic Routes and Models/controllers/admin.js
@@ -1,72 +1,79 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-    //Send data!
-    res.render('admin/edit-product', {
-        pageTitle: "Add product",
-        path: '/admin/add-product',
-        editing: false
-    });
-}
+  //Send data!
+  res.render("admin/edit-product", {
+    pageTitle: "Add product",
+    path: "/admin/add-product",
+    editing: false,
+  });
+};
 
-exports.postAddProduct = (req,res,next) => {
-    //We have to use body-parser to parse the body of the incoming request
-    const title = req.body.title;
-    const imageUrl = req.body.imageUrl;
-    const price = req.body.price;
-    const description = req.body.description;
-    
-    const product = new Product(null, title, imageUrl, description, price);
+exports.postAddProduct = (req, res, next) => {
+  //We have to use body-parser to parse the body of the incoming request
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
 
-    product.save();
-    res.redirect("/");
-}
+  const product = new Product(null, title, imageUrl, description, price);
+
+  product.save();
+  res.redirect("/");
+};
 
 exports.getEditProduct = (req, res, next) => {
-    //It's redundent but it only an example here
-    const editMode = req.query.edit;
-    if (!editMode) {
-        res.redirect('/');
+  //It's redundent but it only an example here
+  const editMode = req.query.edit;
+  if (!editMode) {
+    res.redirect("/");
+  }
+  //Fetching the product
+  const productId = req.params.productId;
+  Product.findById(productId, (product) => {
+    if (product) {
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        editing: editMode,
+        product: product,
+      });
+    } else {
+      res.redirect("/");
     }
-    //Fetching the product
-    const productId = req.params.productId;
-    Product.findById(productId, (product) => {
-        if (product) {
-            res.render('admin/edit-product', {
-                pageTitle: "Edit Product",
-                path: '/admin/edit-product',
-                editing: editMode,
-                product: product
-            });
-        } else {
-            res.redirect('/');
-        }
-    })
-}
+  });
+};
 
 exports.postEditProduct = (req, res) => {
-    const prodId = req.body.productId;
-    const updatedTtile = req.body.title;
-    const updatedImageUrl = req.body.imageUrl;
-    const updatedDescription = req.body.description;
-    const updatedPrice = req.body.price;
+  const prodId = req.body.productId;
+  const updatedTtile = req.body.title;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDescription = req.body.description;
+  const updatedPrice = req.body.price;
 
-    const updatedProduct = new Product(prodId, updatedTtile, updatedImageUrl, updatedDescription, updatedPrice);
-    updatedProduct.save();
-    res.redirect('/admin/products');
-     
+  const updatedProduct = new Product(
+    prodId,
+    updatedTtile,
+    updatedImageUrl,
+    updatedDescription,
+    updatedPrice
+  );
+  updatedProduct.save();
+  res.redirect("/admin/products");
 };
 
 exports.postDeleteProduct = (req, res) => {
-    const prodId = req.body.productId;
-}
+  const prodId = req.body.productId;
+  Product.deleteById(prodId);
+  res.redirect("/admin/products");
+};
 
 exports.getProducts = (req, res) => {
-    Product.fetchAll((products) => {
-        res.render('admin/products', {
-            prods: products,
-            pageTitle: 'Admin products',
-            path: '/admin/products'
-        })
+  Product.fetchAll((products) => {
+    res.render("admin/products", {
+      prods: products,
+      pageTitle: "Admin products",
+      path: "/admin/products",
     });
-}
+  });
+};
--- 9.Dynamic Routes and Models/models/product.js
+++ 9.Dynamic Routes and Models/models/product.js
@@ -1,69 +1,75 @@
-const fs = require('fs');
-const path = require('path');
-const mainPath = require ('../utils/path');
+const fs = require("fs");
+const path = require("path");
+const mainPath = require("../utils/path");
 
-const pathFile = path.join(mainPath, 'data', 'products.json');
+const pathFile = path.join(mainPath, "data", "products.json");
 
-const getProductsFromFile = callback => {
-    const pathFile = path.join(mainPath, 'data', 'products.json');
-    fs.readFile(pathFile, (error, fileContent) => {
-        if(error) {
-            callback([]);
-        } else {
-            callback(JSON.parse(fileContent));
-        }
-    });
-}
+const Cart = require("./cart");
+
+const getProductsFromFile = (callback) => {
+  const pathFile = path.join(mainPath, "data", "products.json");
+  fs.readFile(pathFile, (error, fileContent) => {
+    if (error) {
+      callback([]);
+    } else {
+      callback(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-    constructor(id, title, imageUrl, description, price) {
-        this.id = id;
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.price = price;
-    }
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-    save() {
-        getProductsFromFile(products => {
-            if (this.id) {
-                //Update an existing product
-                const existingProductIndex = products.findIndex(prod => prod.id === this.id);
-                const updatedProducts = [...products];
-                updatedProducts[existingProductIndex] = this;
-                fs.writeFile(pathFile, JSON.stringify(updatedProducts), (err) => {
-                    console.log(err)
-                });
-            } else {
-                this.id = Math.random().toString();
-                products.push(this);
-                fs.writeFile(pathFile, JSON.stringify(updatedProducts), (err) => {
-                    console.log(err)
-                });   
-            }
+  save() {
+    getProductsFromFile((products) => {
+      if (this.id) {
+        //Update an existing product
+        const existingProductIndex = products.findIndex(
+          (prod) => prod.id === this.id
+        );
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(pathFile, JSON.stringify(updatedProducts), (err) => {
+          console.log(err);
         });
-    }
-
-    static deleteById(id) {
-        getProductsFromFile(products => {
-            const updatedProducts = products.filter(p => p.id !== id);
-            fs.writeFileSync(pathFile, JSON.stringify(updatedProducts), err => {
-                if (!err) {
-                    //Remove from the cart
-                }
-            })
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(pathFile, JSON.stringify(products), (err) => {
+          console.log(err);
         });
-    }
+      }
+    });
+  }
 
-    //We should pass callback because reading file is asynchronous
-    static fetchAll(callback) {
-        getProductsFromFile(callback);
-    }
+  static deleteById(id) {
+    getProductsFromFile((products) => {
+      const product = products.find((prod) => prod.id === id);
+      const updatedProducts = products.filter((p) => p.id !== id);
+      fs.writeFile(pathFile, JSON.stringify(updatedProducts), (err) => {
+        if (!err) {
+          //Remove from the cart
+          Cart.deleteProduct(id, product.price);
+        }
+      });
+    });
+  }
 
-    static findById(id, callback) {
-        getProductsFromFile(products => {
-            const product = products.find(p => p.id === id);
-            callback(product);
-        });
-    }
-}
+  //We should pass callback because reading file is asynchronous
+  static fetchAll(callback) {
+    getProductsFromFile(callback);
+  }
+
+  static findById(id, callback) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => p.id === id);
+      callback(product);
+    });
+  }
+};
