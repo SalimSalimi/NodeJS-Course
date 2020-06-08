76ba11774ea16f84e5eafe6f9b738e7ba74d23f5
SalimSalimi <salim.salimi@protonmail.com>
2020-06-08 22:13:14 +0200

    Initializing Sequelize object to connect to DB

--- 11. Sequelize/utils/database.js
+++ 11. Sequelize/utils/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "sasa42",
+const sequelize = Sequelize("node-complete", "root", "sasa42+", {
+  dialect: "mysql",
+  host: "localhost"
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
